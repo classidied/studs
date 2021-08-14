c19b9eb999fd9f5b24f17abbd58c189314ff1b20
classidied <claire.wang028@gmail.com>
2021-08-14 05:08:08 -0400

    janky chat

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/html/homepage.html');
+  res.sendFile(__dirname + '/html/room.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
