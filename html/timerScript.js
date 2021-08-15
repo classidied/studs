27213bef29a27da5f03d4b62333bfbcf2ebd8396
HeliaSeyedmazhari <heliamazhary@yahoo.com>
2021-08-14 21:25:55 -0400

    adjust

--- html/timerScript.js
+++ html/timerScript.js
@@ -1,13 +1,16 @@
-const startingMin=55;
+const startingMin=0.2;
+const breakMin=0.3;
 let time=startingMin*60;
+let breakTime=breakMin*60;
 
 const countdownEl= document.getElementById('countdown');
 
 setInterval (updateCD,1000);
 
 function updateCD(){
-    if (timer==0){
-        
+    if (time==0){
+        alert("Go take a break! You worked really hard!");
+        time=startingMin*60;
     }
     const minutes=Math.floor(time/60);
     let seconds = time%60;
